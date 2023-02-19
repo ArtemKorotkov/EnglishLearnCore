2febe9dbddcf1968c583c3b11f18888a9242a408
Artem Korotkov <iartems2008@gmail.com>
2023-02-19 19:22:04 +0100

    Add show Folder buttons in AllFoldersView

--- Assets/Source/Serialization/Folder.cs
+++ Assets/Source/Serialization/Folder.cs
@@ -1,10 +1,13 @@
-﻿namespace Source.Serialization
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Source.Serialization
 {
     public struct Folder
     {
         public string Name;
-        public string Progress;
-        public string CountLearned;
-        public Word Word;
+        public int CountLearned;
+        public List<Word> Words;
+        public Progress Progress;
     }
 }
--- Assets/Source/Views/AllFolders/AllFoldersView.cs
+++ Assets/Source/Views/AllFolders/AllFoldersView.cs
@@ -6,25 +6,32 @@ namespace Source
 {
     public class AllFoldersView : MonoBehaviour
     {
-
         [SerializeField] private Transform content;
         [SerializeField] private FolderButton buttonPrefab;
-        
+
         public Window window;
 
         public void DisplayFolders(List<Folder> folders)
         {
+            Clear();
             foreach (var folder in folders)
             {
                 FolderButton folderButton = Instantiate(buttonPrefab, content, false);
-                
+
                 folderButton.Name = folder.Name;
+                folderButton.CountLearned = "Выучено: " + folder.CountLearned + " из " + folder.Words?.Count.ToString();
                 folderButton.Progress = folder.Progress;
-                folderButton.CountLearned = folder.CountLearned;
             }
         }
         
-        
-
+        private void Clear()
+        {
+            var childCount = content.childCount;
+            
+            for (int i = 0; i < childCount; i++)
+            {
+                Destroy(content.GetChild(i).gameObject);
+            }
+        }
     }
 }
